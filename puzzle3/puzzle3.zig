2bbaa999fa439a7cd682786a9eff7dccce0d7770
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2021-12-02 09:44:16 -0500

    puzzle 3

---
+++ puzzle3/puzzle3.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+const CommandType = enum { Forward, Down, Up };
+
+const command_map = std.ComptimeStringMap(CommandType, .{
+    .{ "forward", .Forward },
+    .{ "down", .Down },
+    .{ "up", .Up },
+});
+
+fn readLine(file: std.fs.File, buf: []u8) !?[]u8 {
+    return file.reader().readUntilDelimiter(buf, '\n') catch |err| switch (err) {
+        error.EndOfStream => return null,
+        else => |e| return e,
+    };
+}
+
+const Submarine = struct {
+    hPos: u32 = 0,
+    depth: u32 = 0,
+
+    fn nextCommand(sub: *Submarine, slice: []const u8) !void {
+        var it = std.mem.split(u8, slice, " ");
+        const command = command_map.get(it.next().?).?;
+        const amount = try std.fmt.parseInt(u32, it.next().?, 10);
+        switch (command) {
+            .Forward => sub.hPos += amount,
+            .Down => sub.depth += amount,
+            .Up => sub.depth -= amount,
+        }
+    }
+};
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("input", .{});
+    defer file.close();
+
+    var sub = Submarine {};
+    var buf: [128]u8 = undefined;
+
+    while (try readLine(file, &buf)) |slice| {
+        try sub.nextCommand(slice);
+    }
+
+    std.debug.print("{}\n", .{sub.hPos * sub.depth});
+}
