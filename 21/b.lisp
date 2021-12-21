595f5fde2258df7c15cae14f39eee40b532cfbba
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2021-12-21 12:22:50 -0500

    more implementations of day 21 part two

---
+++ 21/b.lisp
@@ -0,0 +1,47 @@
+; all possible dice rolls
+(defparameter *dice-rolls* (make-array 27))
+
+; calculate dice rolls
+(let ((i 0))
+  (loop for a from 1 to 3
+        do (loop for b from 1 to 3
+                 do (loop for c from 1 to 3
+                          do (setf (aref *dice-rolls* i) (+ a b c))
+                             (incf i)))))
+
+; cache of game states
+(defparameter *game-state-cache* (make-array (* 10 10 21 21) :initial-element nil))
+
+(defun game (p1-pos p1-score p2-pos p2-score)
+  (let* ((cache-index (+ p1-pos
+                         (* p1-score 10)
+                         (* p2-pos 210)
+                         (* p2-score 2100)))
+         (cached-result (aref *game-state-cache* cache-index)))
+    (or
+      cached-result
+      (let ((p1-wins 0) (p2-wins 0))
+        (loop for roll across *dice-rolls*
+              do (let* ((new-pos (mod (+ p1-pos roll) 10))
+                        (new-score (+ p1-score new-pos 1)))
+                   (cond
+                     ((>= new-score 21)
+                       (incf p1-wins))
+                     (t
+                       (let ((wins (game p2-pos p2-score new-pos new-score)))
+                         (incf p1-wins (cdr wins))
+                         (incf p2-wins (car wins)))))))
+        (setf (aref *game-state-cache* cache-index) (cons p1-wins p2-wins))))))
+
+(defun load-one-initial-pos (file)
+  (let ((line (read-line file)))
+    (- (parse-integer (subseq line (- (length line) 2))) 1)))
+
+(defun load-initial-state (filename)
+  (with-open-file (file filename)
+    (values (load-one-initial-pos file)
+            (load-one-initial-pos file))))
+
+(multiple-value-bind (p1-pos p2-pos) (load-initial-state "input")
+  (let ((wins (game p1-pos 0 p2-pos 0)))
+    (format t "~a~%" (max (car wins) (cdr wins)))))
---
+++ 21/b.zig
@@ -0,0 +1,89 @@
+const std = @import("std");
+
+const dice_rolls = blk: {
+    var rolls: [27]u8 = undefined;
+    var i = 0;
+    // calculate dice rolls
+    var a = 1;
+    while (a <= 3) : (a += 1) {
+        var b = 1;
+        while (b <= 3) : (b += 1) {
+            var c = 1;
+            while (c <= 3) : (c += 1) {
+                rolls[i] = a + b + c;
+                i += 1;
+            }
+        }
+    }
+    break :blk rolls;
+};
+
+const Cache = [10 * 10 * 21 * 21][2]u64;
+
+fn cacheIndex(p1_pos: u8, p1_score: u8, p2_pos: u8, p2_score: u8) u16 {
+    return @as(u16, p1_pos)
+        + (@as(u16, p1_score) * 10)
+        + (@as(u16, p2_pos) * 210)
+        + (@as(u16, p2_score) * 2100);
+}
+
+fn game(p1_pos: u8, p1_score: u8, p2_pos: u8, p2_score: u8, cache: *Cache) [2]u64 {
+    const index = cacheIndex(p1_pos, p1_score, p2_pos, p2_score);
+    const cached = cache[index];
+    if (cached[0] != 0 or cached[1] != 0) {
+        return cached;
+    } else {
+        var wins = [2]u64{ 0, 0 };
+        for (dice_rolls) |roll| {
+            const new_pos = (p1_pos + roll) % 10;
+            const new_score = p1_score + new_pos + 1;
+            if (new_score >= 21) {
+                wins[0] += 1;
+            } else {
+                const new_wins = game(p2_pos, p2_score, new_pos, new_score, cache);
+                wins[1] += new_wins[0];
+                wins[0] += new_wins[1];
+            }
+        }
+        cache[index] = wins;
+        return wins;
+    }
+}
+
+fn readInitialState(filename: []const u8) ![2]u8 {
+    var buf: [64]u8 = undefined;
+
+    const input = blk: {
+        const file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+        break :blk buf[0..try file.reader().readAll(&buf)];
+    };
+
+    var result: [2]u8 = undefined;
+
+    var lines = std.mem.split(u8, input, "\n");
+    for (result) |*pos| {
+        const line = lines.next().?;
+        const space = std.mem.lastIndexOfScalar(u8, line, ' ').? + 1;
+        pos.* = (try std.fmt.parseUnsigned(u8, line[space..], 10)) - 1;
+    }
+
+    return result;
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}) {};
+    defer _ = gpa.deinit();
+    const allocator = &gpa.allocator;
+
+    const pos = try readInitialState("input");
+
+    const cache = try allocator.create(Cache);
+    defer allocator.destroy(cache);
+
+    @memset(@ptrCast([*]u8, cache), 0, @sizeOf(Cache));
+
+    const wins = game(pos[0], 0, pos[1], 0, cache);
+
+    std.debug.print("{}\n", .{std.math.max(wins[0], wins[1])});
+}
